9720ecfb78fc49f1a28a85ef879141f08f666932
Kelsey Low <kklowlow@gmail.com>
2019-07-24 21:23:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 function scuberGreetingForFeet(feet){
-  // Write your code here!
   if (feet <= 400) {
-    
-  }
+    return 'This one is on me!';
+  } else if
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+  
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  
 }
